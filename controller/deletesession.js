edfd30d9adb27d6aeb14a62e87ad7de9487bb561
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-08 14:50:38 +0530

    `Added category ID validation and removal logic in DeleteSection controller`

--- controller/deletesession.js
+++ controller/deletesession.js
@@ -3,21 +3,34 @@ var model = require("../model/deletesession");
 module.exports.DeleteSection = async (req, res) => {
 
     try {
+        var c_id = req.body.c_id;
+
         var b_id = req.body.b_id;
         var p_id = req.body.p_id;
-        var c_id = req.body.c_id;
         var ct_id = req.body.ct_id;
         var ua_id = req.body.ua_id;
 
+        if (c_id) {
+            let checkcategory = await model.CheckCategoryQuery(c_id);
+            if (checkcategory.length == 0) {
+                return res.send({
+                    result: false,
+                    message: "Category not found"
+                });
+            } else {
+
+                var deletesection = await model.RemoveCategoryQuery(c_id);
+
+            }
+        }
         if (b_id) {
             var deletesection = await model.RemoveBannerQuery(b_id);
         }
         if (p_id) {
             var deletesection = await model.RemoveproductQuery(p_id);
         }
-        if (c_id) {
-            var deletesection = await model.RemoveCategoryQuery(c_id);
-        }
+
+
         if (ct_id) {
             var deletesection = await model.RemoveCartQuery(ct_id);
         }
