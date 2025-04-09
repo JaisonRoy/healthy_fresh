64ebec38c722d59d28d8d0fe75bf67d35e7cc361
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-09 14:09:00 +0530

    `Update subCategory model to match database schema changes`

--- model/subCategory.js
+++ model/subCategory.js
@@ -3,23 +3,23 @@ var util = require("util");
 const query = util.promisify(db.query).bind(db);
 
 module.exports.AddSubCategoryQuery = async (name, imagepath, category_id) => {
-    var Query = `insert into sub_category (name,image,category_id) values (?,?,?) `;
+    var Query = `insert into sub_category (sc_name,sc_image,sc_category_id) values (?,?,?) `;
     var data = query(Query, [name, imagepath, category_id]);
     return data;
 }
 
 module.exports.CheckAlreadyExists = async (name) => {
-    var Query = `select * from sub_category where name = ?`
+    var Query = `select * from sub_category where sc_name = ?`
     return await query(Query, [name])
 }
 
 module.exports.FindSubCategory = async (subCategory_id) => {
-    var Query = `select * from sub_category where id=?`
+    var Query = `select * from sub_category where sc_id=?`
     return await query(Query, [subCategory_id])
 }
 
 module.exports.UpdateSubCategoryData = async (name, imagepath, category_id, subCategory_id) => {
-    let Query = `update sub_category set name=?,image=?,category_id=? where id=?`
+    let Query = `update sub_category set sc_name=?,sc_image=?,sc_category_id=? where sc_id=?`
     return await query(Query, [name, imagepath, category_id, subCategory_id])
 }
 
@@ -29,6 +29,6 @@ module.exports.ListAllSubcategories = async () => {
 }
 
 module.exports.DeleteSubCategory = async (subCategory_id) => {
-    let Query = `delete from sub_category where id=?`
+    let Query = `delete from sub_category where sc_id=?`
     return await query(Query, [subCategory_id])
 }
